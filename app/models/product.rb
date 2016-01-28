c818173bbbd388e015fe7feaa1bd5916ce17b2c9
Lyubomr <s.lyubomyr@gmail.com>
2016-01-28 16:50:05 -0500

    Adding price in dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   validates :description, :name, presence: true # <= What's validation and presence?
   validates :price_in_cents, numericality: {only_interger: true} # <= What's numericality and only_interger?
+
+  def formatted_price
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars) # <= What's all this business?
+  end
 end
