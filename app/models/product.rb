af1596b2f4c8c37594777ccff13482a6446f55ae
Lyubomr <s.lyubomyr@gmail.com>
2016-01-28 16:02:37 -0500

    Writing product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  validates :description, :name, presence: true # <= What's validation and presence?
+  validates :price_in_cents, numericality: {only_interger: true} # <= What's numericality and only_interger?
 end
